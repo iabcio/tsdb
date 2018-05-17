a493ff3d827dec15eb0a027806df70406deead90
shuchen <h@iabc.io>
2018-05-17 09:51:43 +0800

    shuchen : more filter

--- opentsdb-client/src/main/java/io/iabc/tsdb/opentsdb/client/domain/TsdbQuery.java
+++ opentsdb-client/src/main/java/io/iabc/tsdb/opentsdb/client/domain/TsdbQuery.java
@@ -316,6 +316,50 @@ public class TsdbQuery implements Serializable {
 
     public static class Filter {
 
+        /**
+         * tagv的过滤规则: 精确匹配多项迭代值，多项迭代值以'|'分隔，大小写敏感
+         */
+        public static String FILTER_TYPE_LITERAL_OR = "literal_or";
+
+        /**
+         * tagv的过滤规则: 精确匹配多项迭代值，多项迭代值以'|'分隔，忽略大小写
+         */
+        public static String FILTER_TYPE_ILITERAL_OR = "iliteral_or";
+
+        /**
+         * tagv的过滤规则: 通配符取非匹配，大小写敏感
+         */
+        public static String FILTER_TYPE_NOT_LITERAL_OR = "not_literal_or";
+
+        /**
+         * tagv的过滤规则: 通配符取非匹配，忽略大小写
+         */
+        public static String FILTER_TYPE_NOT_ILITERAL_OR = "not_iliteral_or";
+
+        /**
+         * tagv的过滤规则: 通配符匹配，大小写敏感
+         */
+        public static String FILTER_TYPE_WILDCARD = "wildcard";
+
+        /**
+         * tagv的过滤规则: 通配符匹配，忽略大小写
+         */
+        public static String FILTER_TYPE_IWILDCARD = "iwildcard";
+
+        /**
+         * tagv的过滤规则: 正则表达式匹配
+         */
+        public static String FILTER_TYPE_REGEXP = "regexp";
+
+        /**
+         * tagv的过滤规则:
+         * <p/>
+         * Skips any time series with the given tag key, regardless of the value.
+         * This can be useful for situations where a metric has inconsistent tag sets.
+         * NOTE: The filter value must be null or an empty string
+         */
+        public static String FILTER_TYPE_NOT_KEY = "not_key";
+
         private String type;
         private String tagk;
         private String filter;
@@ -339,16 +383,36 @@ public class TsdbQuery implements Serializable {
         protected Filter() {
         }
 
+        public static FilterBuilder literalOr(String tagk, String... filters) {
+            return new FilterBuilder(FILTER_TYPE_LITERAL_OR, tagk, filters);
+        }
+
         public static FilterBuilder iliteralOr(String tagk, String... filters) {
-            return new FilterBuilder("iliteral_or", tagk, filters);
+            return new FilterBuilder(FILTER_TYPE_ILITERAL_OR, tagk, filters);
+        }
+
+        public static FilterBuilder notLiteralOr(String tagk, String... filters) {
+            return new FilterBuilder(FILTER_TYPE_NOT_LITERAL_OR, tagk, filters);
+        }
+
+        public static FilterBuilder notIliteralOr(String tagk, String... filters) {
+            return new FilterBuilder(FILTER_TYPE_NOT_ILITERAL_OR, tagk, filters);
         }
 
         public static FilterBuilder wildcard(String tagk, String... filters) {
-            return new FilterBuilder("wildcard", tagk, filters);
+            return new FilterBuilder(FILTER_TYPE_WILDCARD, tagk, filters);
+        }
+
+        public static FilterBuilder iwildcard(String tagk, String... filters) {
+            return new FilterBuilder(FILTER_TYPE_IWILDCARD, tagk, filters);
         }
 
         public static FilterBuilder regexp(String tagk, String... filters) {
-            return new FilterBuilder("regexp", tagk, filters);
+            return new FilterBuilder(FILTER_TYPE_REGEXP, tagk, filters);
+        }
+
+        public static FilterBuilder notKey(String tagk) {
+            return new FilterBuilder(FILTER_TYPE_NOT_KEY, tagk, "");
         }
 
         public static class FilterBuilder {
